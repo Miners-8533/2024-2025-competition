11b5a2d8e96ed7b2addbe5b95318a316029d2072
Jack Jenkins <jack@brightbraindesign.com>
2024-10-21 19:30:34 -0400

    fix: iterate basket trajectory

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -7,7 +7,7 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
@@ -15,11 +15,15 @@ public class MeepMeepTesting {
                 .build();
 
 
-        Pose2d initialPose = new Pose2d(-24,-63, Math.toRadians(90));
+        Pose2d initialPose = new Pose2d(-18.5,-63, Math.toRadians(90));
 
         Pose2d scoreChamber = new Pose2d(-8,-31, Math.toRadians(90));
 
+        Pose2d avoidSubmersibleGusset = new Pose2d(-35, -35, Math.toRadians(150));
+
         Pose2d scoreRedBasketLeft = new Pose2d(-41,-60, Math.toRadians(180));
+
+        Pose2d scoreHighBasket = new Pose2d(-48, -48, Math.toRadians(225));
 
         Pose2d firstSpikeMark = new Pose2d(-36,-22.25, Math.toRadians(180));
 
@@ -27,11 +31,9 @@ public class MeepMeepTesting {
                 .splineToLinearHeading(scoreChamber, Math.toRadians(90))
                 .waitSeconds(2)
                 .setReversed(true)
-                .splineToLinearHeading(scoreRedBasketLeft, Math.toRadians(180))
+                .splineToLinearHeading(avoidSubmersibleGusset, Math.toRadians(180))
                 .waitSeconds(2)
-                .splineToLinearHeading(firstSpikeMark, Math.toRadians(90))
-                .waitSeconds(2)
-                .strafeTo(scoreRedBasketLeft.position)
+                .splineToLinearHeading(scoreHighBasket, Math.toRadians(180))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
