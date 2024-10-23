d364be17d83e057b344ddd91419083cdd4ed7416
Jack Jenkins <jack@brightbraindesign.com>
2024-10-22 21:16:48 -0400

    feat: add second spike mark and park poses

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -19,21 +19,31 @@ public class MeepMeepTesting {
 
         Pose2d scoreChamber = new Pose2d(-8,-31, Math.toRadians(90));
 
-        Pose2d avoidSubmersibleGusset = new Pose2d(-35, -35, Math.toRadians(150));
+//        Pose2d avoidSubmersibleGusset = new Pose2d(-35, -35, Math.toRadians(150));
 
-        Pose2d scoreRedBasketLeft = new Pose2d(-41,-60, Math.toRadians(180));
+//        Pose2d scoreRedBasketLeft = new Pose2d(-41,-60, Math.toRadians(180));
 
         Pose2d scoreHighBasket = new Pose2d(-48, -48, Math.toRadians(225));
 
-        Pose2d firstSpikeMark = new Pose2d(-36,-22.25, Math.toRadians(180));
+        Pose2d firstSpikeMark = new Pose2d(-35, -35, Math.toRadians(150));
+
+        Pose2d secondSpikeMark = new Pose2d(-45, -35, Math.toRadians(150));
+
+        Pose2d parkNearSubmersible = new Pose2d(-24, -12.5, 0);
 
         myBot.runAction(myBot.getDrive().actionBuilder(initialPose)
                 .splineToLinearHeading(scoreChamber, Math.toRadians(90))
                 .waitSeconds(2)
                 .setReversed(true)
-                .splineToLinearHeading(avoidSubmersibleGusset, Math.toRadians(180))
+                .splineToLinearHeading(firstSpikeMark, Math.toRadians(180))
                 .waitSeconds(2)
                 .splineToLinearHeading(scoreHighBasket, Math.toRadians(180))
+                .waitSeconds(2)
+                .splineToLinearHeading(secondSpikeMark,Math.toRadians(180))
+                .waitSeconds(2)
+                .splineToLinearHeading(scoreHighBasket, Math.toRadians(180))
+                .waitSeconds(2)
+                .splineToLinearHeading(parkNearSubmersible, Math.toRadians(0))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
