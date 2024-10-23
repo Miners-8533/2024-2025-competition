30c0c3cfdbf67247b9be43895f1d71a90cf63779
Jack Jenkins <jack@brightbraindesign.com>
2024-10-22 20:42:47 -0400

    feat: split test bot into its own module

---
+++ TeamCodeTestBot/src/main/java/org/firstinspires/ftc/teamcodetestbot/opmodes/testing/AServo.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcodetestbot.opmodes.testing;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class AServo {
+    private Servo aServo;
+
+    public AServo(HardwareMap hardwareMap) {
+        aServo = hardwareMap.get(Servo.class, "a_servo");
+    }
+
+    public Action OpenServo() {
+        return new Action() {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    aServo.setPosition(1);
+                    initialized = true;
+                }
+
+                double pos = aServo.getPosition();
+                packet.put("aServo position", pos);
+                return false;
+            }
+        };
+    }
+
+    public Action CloseServo() {
+        return new Action() {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    aServo.setPosition(0.25);
+                    initialized = true;
+                }
+
+                double pos = aServo.getPosition();
+                packet.put("aServo position", pos);
+                return false;
+            }
+        };
+    }
+}
