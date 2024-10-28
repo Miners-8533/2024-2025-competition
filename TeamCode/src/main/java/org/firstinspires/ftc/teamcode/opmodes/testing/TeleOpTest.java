7140eadda33610795af8ca33c15623c76f0a5f46
rise <mkblaser@users.noreply.github.com>
2024-10-28 19:34:26 -0400

    add testing TeleOpTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/TeleOpTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes.testing;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.subsystems.Robot;
+
+@TeleOp(name="TeleOp Test")
+public class TeleOpTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        Robot robot = new Robot(hardwareMap, gamepad1, gamepad2);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            robot.updateTeleOp(telemetry);
+        }
+    }
+}
