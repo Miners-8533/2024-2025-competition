2899cd156358ebf5c9d091995258e54b0c50da1e
Jack Jenkins <jack@brightbraindesign.com>
2024-10-31 16:09:23 -0400

    feat: intial observation zone auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/AutonBasicTrajectoryObservationSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/AutonBasicTrajectoryObservationSide.java
@@ -19,41 +19,54 @@ public class AutonBasicTrajectoryObservationSide extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        Pose2d initialPose = new Pose2d(-16,-62, Math.toRadians(90));
-
-        Pose2d scoreChamber = new Pose2d(-8,-29, Math.toRadians(90));
-
-        Pose2d scoreHighBasket = new Pose2d(-53, -49, Math.toRadians(225));
-
-        Pose2d firstSpikeMark = new Pose2d(-33, -33, Math.toRadians(150));
-
-        Pose2d secondSpikeMark = new Pose2d(-39, -30, Math.toRadians(150));
-
-        Pose2d parkNearSubmersible = new Pose2d(-15, -6.5, Math.toRadians(180));
+        Pose2d initialPose = new Pose2d(16,-62, Math.toRadians(90));
+        Pose2d scoreChamber = new Pose2d(8,-29, Math.toRadians(90));
+        Pose2d firstSpikeMark = new Pose2d(33, -33, Math.toRadians(30));
+        Pose2d secondSpikeMark = new Pose2d(39, -30, Math.toRadians(30));
+        Pose2d observationZonePose = new Pose2d(47, -58, Math.toRadians(315));
+        Pose2d chamberTwoPose = new Pose2d(5,-31, Math.toRadians(90));
+        Pose2d chamberThreePose = new Pose2d(2,-31, Math.toRadians(90));
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
         Robot robot = new Robot(hardwareMap,gamepad1,gamepad2);
 
-        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+        TrajectoryActionBuilder scoreChamberTab = drive.actionBuilder(initialPose)
                 .splineToLinearHeading(scoreChamber, Math.toRadians(90));
 
-        TrajectoryActionBuilder tab2 = drive.actionBuilder(scoreChamber)
+        TrajectoryActionBuilder firstSpikeMarkTab = drive.actionBuilder(scoreChamber)
                 .setReversed(true)
-                .splineToLinearHeading(firstSpikeMark, Math.toRadians(180));
+                .splineToLinearHeading(firstSpikeMark, Math.toRadians(90));
 
-        TrajectoryActionBuilder tab3  = drive.actionBuilder(firstSpikeMark)
-                .splineToLinearHeading(scoreHighBasket, Math.toRadians(180));
+        TrajectoryActionBuilder firstObservationTab = drive.actionBuilder(firstSpikeMark)
+                .splineToLinearHeading(observationZonePose, Math.toRadians(270));
 
-        TrajectoryActionBuilder tab4 = drive.actionBuilder(scoreHighBasket)
+        TrajectoryActionBuilder secondSpikeMarkTab = drive.actionBuilder(observationZonePose)
+                .setReversed(true)
                 .splineToLinearHeading(secondSpikeMark,Math.toRadians(180));
 
-        TrajectoryActionBuilder tab5 = drive.actionBuilder(secondSpikeMark)
-                .splineToLinearHeading(scoreHighBasket, Math.toRadians(180));
+        TrajectoryActionBuilder secondObservationZoneTab = drive.actionBuilder(secondSpikeMark)
+                .splineToLinearHeading(observationZonePose, Math.toRadians(180));
 
-        TrajectoryActionBuilder tab6 = drive.actionBuilder(scoreHighBasket)
+        TrajectoryActionBuilder turnToSpecimenTab = drive.actionBuilder(observationZonePose)
+                .turnTo(Math.toRadians(270))
+                .lineToY(initialPose.position.y);
+
+        TrajectoryActionBuilder secondScoreChamberTab = drive.actionBuilder(new Pose2d(observationZonePose.position.x, initialPose.position.y, Math.toRadians(270)))
                 .setReversed(true)
-                .splineToLinearHeading(parkNearSubmersible, Math.toRadians(0));
+                .splineToLinearHeading(chamberTwoPose, Math.toRadians(90));
+
+        TrajectoryActionBuilder thirdObservationZoneTab = drive.actionBuilder(chamberTwoPose)
+                .setReversed(true)
+                .splineToLinearHeading(observationZonePose, Math.toRadians(270));
+
+        TrajectoryActionBuilder thirdScoreChamberTab = drive.actionBuilder(new Pose2d(observationZonePose.position.x, initialPose.position.y, Math.toRadians(270)))
+                .setReversed(true)
+                .splineToLinearHeading(chamberThreePose, Math.toRadians(90));
+
+        TrajectoryActionBuilder parkInObservationZone = drive.actionBuilder(chamberThreePose)
+                .setReversed(true)
+                .splineToLinearHeading(observationZonePose, Math.toRadians(270));
 
         // Wait for the game to start (driver presses START)
         waitForStart();
@@ -61,7 +74,7 @@ public class AutonBasicTrajectoryObservationSide extends LinearOpMode {
         Actions.runBlocking(new SequentialAction(
                 new ParallelAction(
                         robot.autonStart(),
-                        tab1.build()
+                        scoreChamberTab.build()
                 ),
                 robot.scoreSpecimen(),
                 new ParallelAction(
@@ -69,47 +82,53 @@ public class AutonBasicTrajectoryObservationSide extends LinearOpMode {
                                 robot.goToReadyPose(),
                                 robot.floorAcquire()
                         ),
-                        tab2.build()
+                        firstSpikeMarkTab.build()
                 ),
                 robot.floorAcquireReach(),
                 new ParallelAction(
-                        tab3.build(),
-                        robot.prepareScoreHighBasket()
+                        firstObservationTab.build(),
+                        robot.goToReadyPose()
                 ),
-                robot.highBasketReach(),
                 new ParallelAction(
-                        new SleepAction(0.3),
-                        robot.scoreHighBasket()
+                        robot.outakeSampleGround(),
+                        new SleepAction(0.3)
                 ),
                 new ParallelAction(
                         new SequentialAction(
-                                robot.prepareScoreHighBasket(),
                                 robot.goToReadyPose(),
                                 robot.floorAcquire()
                         ),
-                        new SequentialAction(
-                                tab4.build(),
-                                new SleepAction(0.5)
-                        )
+                        secondSpikeMarkTab.build()
                 ),
                 robot.floorAcquireReach(),
                 new ParallelAction(
-                        robot.prepareScoreHighBasket(),
-                        tab5.build()
-                ),
-                robot.highBasketReach(),
-                new ParallelAction(
-                        new SleepAction(0.3),
-                        robot.scoreHighBasket()
+                        secondObservationZoneTab.build(),
+                        robot.goToReadyPose()
                 ),
                 new ParallelAction(
-                        new SequentialAction(
-                                robot.prepareScoreHighBasket(),
-                                robot.goToReadyPose()
-                        ),
-                        tab6.build()
+                        robot.outakeSampleGround(),
+                        new SleepAction(0.3)
                 ),
-                robot.goToReadyPose()
+                turnToSpecimenTab.build(),
+                new ParallelAction(
+                        robot.autonStart(),
+                        secondScoreChamberTab.build()
+                ),
+                robot.scoreSpecimen(),
+                new ParallelAction(
+                        thirdObservationZoneTab.build(),
+                        robot.goToReadyPose()
+                ),
+                turnToSpecimenTab.build(),
+                new ParallelAction(
+                        robot.autonStart(),
+                        thirdScoreChamberTab.build()
+                ),
+                robot.scoreSpecimen(),
+                new ParallelAction(
+                        robot.goToReadyPose(),
+                        thirdObservationZoneTab.build()
+                )
         ));
     }
 }
